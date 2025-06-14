ea65b9a107a6f65628923d0404cb4c54b6cc3327
Thiago Rocha <mrthiago09@gmail.com>
2025-06-14 18:11:41 -0300

    teste controalr robo pygame

---
+++ RoboPlayer.py
@@ -0,0 +1,68 @@
+import pygame
+import numpy as np
+from multiprocessing import Lock, shared_memory,Value,Manager
+from auxiliar import verificar_posicao_valida,realizar_acao_player
+
+class RoboPlayer():    
+    def __init__(self, id, posicao,forca,velocidade):
+        self.status = 1 # 1 = vivo, 0 = inativo
+        self.energia = 100
+        self.posicao = posicao
+        self.id = id
+        self.velocidade = velocidade
+        self.forca = forca
+        poder = 2* self.forca * self.velocidade
+    
+    def controlador_robo(self,shm,linhas,colunas):        
+        # Configuração do Pygame
+        global robos_array, grid_mutex, baterias_dict_mutex, robos_dict_mutex
+        running = True
+        while running:
+            pygame.init()
+            pygame.display.set_mode((200, 200))
+            clock = pygame.time.Clock()
+
+            #Tratamento de eventos 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            
+            # Captura estado contínuo do teclado
+            keys = pygame.key.get_pressed()
+            
+            # Calcula nova posição
+            if keys[pygame.K_UP] or keys[pygame.K_w]:
+                nova_posicao = (self.posicao[0],self.posicao[1]+1)
+                #Modificar o tabuleiro compartilhado
+                grid_mutex.acquire()
+                tabuleiro_shm = np.ndarray((linhas, colunas), dtype=np.int8, buffer=shm.buf)
+                if(verificar_posicao_valida(nova_posicao, tabuleiro_shm)):
+                    realizar_acao_player(robo=self,tabuleiro=tabuleiro_shm,posicao=nova_posicao, baterias_dict_mutex=baterias_dict_mutex)
+                grid_mutex.release()
+            elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
+                nova_posicao = (self.posicao[0],self.posicao[1]-1)
+                #Modificar o tabuleiro compartilhado
+                grid_mutex.acquire()
+                tabuleiro_shm = np.ndarray((linhas, colunas), dtype=np.int8, buffer=shm.buf)
+                if(verificar_posicao_valida(nova_posicao, tabuleiro_shm)):
+                    realizar_acao_player(robo=self,tabuleiro=tabuleiro_shm,posicao=nova_posicao, baterias_dict_mutex=baterias_dict_mutex)
+                grid_mutex.release()
+            elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+                nova_posicao = (self.posicao[0]-1,self.posicao[1])
+                #Modificar o tabuleiro compartilhado
+                grid_mutex.acquire()
+                tabuleiro_shm = np.ndarray((linhas, colunas), dtype=np.int8, buffer=shm.buf)
+                if(verificar_posicao_valida(nova_posicao, tabuleiro_shm)):
+                    realizar_acao_player(robo=self,tabuleiro=tabuleiro_shm,posicao=nova_posicao, baterias_dict_mutex=baterias_dict_mutex)
+                grid_mutex.release()
+            elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+                nova_posicao = (self.posicao[0]+1,self.posicao[1])
+                #Modificar o tabuleiro compartilhado
+                grid_mutex.acquire()
+                tabuleiro_shm = np.ndarray((linhas, colunas), dtype=np.int8, buffer=shm.buf)
+                if(verificar_posicao_valida(nova_posicao, tabuleiro_shm)):
+                    realizar_acao_player(robo=self,tabuleiro=tabuleiro_shm,posicao=nova_posicao, baterias_dict_mutex=baterias_dict_mutex)
+                grid_mutex.release()
+            clock.tick(30)
+        pygame.quit()    
+        
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import numpy as np
-from multiprocessing import Lock, shared_memory
-from auxiliar import spawn_valores_aleatorios
+import os
+from multiprocessing import Lock, shared_memory,Value,Manager,Process
+from auxiliar import spawn_valores_aleatorios,inicializar_locks
 from visualizador_pygame import viewer
+from RoboPlayer import RoboPlayer
 
+def iniciar_robo_jogador(shm, linhas, colunas, robos_array, grid_mutex, baterias_dict_mutex, robos_dict_mutex):
+    id_robo = os.getpid()
+    robo_jogador = RoboPlayer(id=99,posicao=[20, 10], forca=10, velocidade=5)
+     
+    grid_mutex.acquire()
+    tabuleiro_shm = np.ndarray((linhas, colunas), dtype=np.int8, buffer=shm.buf)
+    tabuleiro_shm[robo_jogador.posicao[0], robo_jogador.posicao[1]] = robo_jogador.id
+    grid_mutex.release()
+    
+    robos_array.append(robo_jogador) # Adiciona o robô ao array compartilhado
+    robo_jogador.controlador_robo(shm, linhas, colunas) # Inicia o controlador do robô jogador
+    
 #Configurações do jogo
 linhas, colunas = 40, 20
-
-#Inicialização dos mutexes
-
 
 def create_grid():
 
@@ -16,7 +27,8 @@ def create_grid():
 
     #definindo localização dos objetos
     spawn_valores_aleatorios(tabuleiro, 80, 1) #barreiras
-    spawn_valores_aleatorios(tabuleiro, 40, 2) #energia
+    global posicoes_baterias
+    posicoes_baterias = spawn_valores_aleatorios(tabuleiro, 40, 2) #energia
     tabuleiro[15, 15] = 10   # robô comum
     tabuleiro[20, 10] = 99   # robô jogador
 
@@ -30,9 +42,23 @@ def create_grid():
     return shm
 
 if __name__ == "__main__":
+    #Variaveis compartilhadas
+    manager = Manager() #Controla o ciclo de vida das variaveis compartilhadas
+    posicoes_baterias = [] # Lista preenchida na funcao create_grid
     shm = create_grid()
+    robos_array = manager.list()
+    jogo_finalizado = manager.Value('b', False)
+    
+    #Inicialização dos mutexes 
+    grid_mutex = manager.Lock()
+    baterias_dict_mutex,robos_dict_mutex = inicializar_locks(manager, posicoes_baterias, robos_array)
     try:
         viewer(linhas, colunas, shm)
+        # Inicia o robô jogador
+        robo_jogador = Process(target=iniciar_robo_jogador, args=(shm, linhas, colunas, robos_array, grid_mutex, baterias_dict_mutex, robos_dict_mutex))
+        robo_jogador.start()
+        robo_jogador.join()
     except KeyboardInterrupt:
         shm.close()
         shm.unlink()
+        manager.shutdown()
