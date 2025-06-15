c599959653addadf8108fbdac11467565bca0871
Thiago Rocha <mrthiago09@gmail.com>
2025-06-15 02:23:10 -0300

    adicionando funcoes auxiliares

--- auxiliar.py
+++ auxiliar.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 def spawn_valores_aleatorios(tabuleiro: np.ndarray, quantidade: int, valor: int):
+    posicoes_geradas = []
     livres = np.argwhere(tabuleiro == 0)
     if quantidade > len(livres):
         raise ValueError("Quantidade maior que espaços livres disponíveis no tabuleiro.")
@@ -8,3 +9,55 @@ def spawn_valores_aleatorios(tabuleiro: np.ndarray, quantidade: int, valor: int)
     escolhas = livres[np.random.choice(len(livres), quantidade, replace=False)]
     for pos in escolhas:
         tabuleiro[pos[0], pos[1]] = valor
+        posicoes_geradas.append((pos[0], pos[1]))
+    
+    return posicoes_geradas
+
+def inicializar_locks(manager, posicoes_baterias, robos_array):
+    """
+    Os mutexes são usados para controlar o acesso concorrente às baterias e a alteracao das caracteristicas dos robôs
+    o mutex de cada bateria eh acessado com a chave "x,y" onde x e y são as coordenadas da bateria
+    o mutex de cada robô eh acessado com a chave "id" onde id é o identificador do robô
+    """
+    
+    baterias_dict_mutex = manager.dict()
+    robos_dict_mutex = manager.dict()
+
+    for posicao_bateria in posicoes_baterias:
+        key = f"{posicao_bateria[0]}{posicao_bateria[1]}"
+        baterias_dict_mutex[key] = manager.Lock()
+
+    for robo in robos_array:
+        key = str(robo.id)
+        robos_dict_mutex[key] = manager.Lock()
+
+    return baterias_dict_mutex, robos_dict_mutex
+
+def verificar_posicao_valida(posicao,tabuleiro)->bool:
+    linhas = 40
+    colunas = 20
+    pos_x = posicao[0]
+    pos_y = posicao[1]
+    #Verifica se a posicao esta dentro dos limites do tabuleiro
+    if pos_x < 0 or pos_x >= linhas or pos_y < 0 or pos_y >= colunas:
+        return False
+    
+    valor_do_tabuleiro = tabuleiro[pos_x, pos_y]
+    #Verifica se o valor do tabuleiro na posicao eh valido
+    if(valor_do_tabuleiro == 0): # Espaco livre
+        return True
+    elif(valor_do_tabuleiro == 1): #Barreira
+        return False
+    else: #Bateria ou robô
+        return True
+    
+def realizar_acao_player(robo,tabuleiro,nova_posicao,baterias_dict_mutex):
+    valor_tabuleiro = tabuleiro[nova_posicao[0], nova_posicao[1]]
+    if valor_tabuleiro == 0:  # Espaço livre
+        tabuleiro[robo.posicao[0], robo.posicao[1]] = 0  # Limpa a posição antiga
+        robo.posicao = nova_posicao
+        tabuleiro[nova_posicao[0], nova_posicao[1]] = robo.id  # Atualiza a nova posição do robô
+    elif valor_tabuleiro == 2:  # Bateria
+        pass  # Implementar lógica de coleta de bateria e organizacao dos mutex
+    elif valor_tabuleiro == 99:  # Robô jogador
+        pass  # Implementar lógica de interação entre robos
