be440133657b49625666ff641230bec484cedc2a
joao <joao_aguiarcosta@hotmail.com>
2025-06-15 01:22:18 -0300

    housekeeping energy print

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-from multiprocessing import shared_memory, Process
+from multiprocessing import shared_memory, Process, Value
 import numpy as np
 from multiprocessing import Lock, shared_memory
 from auxiliar import spawn_valores_aleatorios
 from visualizador_pygame import viewer
 from robot import Robot
 from random import randint
+import time
 
-#Configurações do jogo
 linhas, colunas = 40, 20
+
+grid_mutex = Lock()
+robots_mutex = Lock()
+
+game_over_flag = Value('i', 0)
+
+robot_dtype = np.dtype([
+        ('id', np.int32),
+        ('strength', np.int32),
+        ('energy', np.int32),
+        ('speed', np.int32),
+        ('pos', np.int32, (2,)),
+        ('status', np.int8)
+    ])
 
 def create_grid():
 
     tabuleiro = np.zeros((linhas, colunas), dtype=np.int8)
 
     spawn_valores_aleatorios(tabuleiro, 40, 2) 
+    spawn_valores_aleatorios(tabuleiro, 40, 1) 
     tabuleiro[15, 15] = 10   
     tabuleiro[20, 10] = 99   
 
@@ -23,30 +38,41 @@ def create_grid():
     return shm
 
 def spawn_robots(num_robots):
-    robot_dtype = np.dtype([
-        ('id', np.int32),
-        ('strength', np.int32),
-        ('energy', np.int32),
-        ('speed', np.int32),
-        ('pos', np.int32, (2,)),
-        ('status', np.int8)
-    ])
-
     robots_shm = shared_memory.SharedMemory(name="robots", create=True, size=robot_dtype.itemsize * num_robots)
     robots = np.ndarray((num_robots,), dtype=robot_dtype, buffer=robots_shm.buf)
-    for _ in range(num_robots):
-        p = Process(target=Robot(randint(1, 10), randint(10, 100), randint(1, 5), status="vivo"))
+
+
+    processes = []
+
+    for i in range(num_robots):
+        strength = randint(1, 10)
+        energy = randint(10, 100)
+        speed = randint(1, 5)
+        pos = (randint(0, linhas - 1), randint(0, colunas - 1))
+
+        robots[i] = (i, strength, energy, speed, pos, 1)
+
+        p = Process(target=Robot(strength, energy, speed, "vivo", i, "robots", robots_mutex, game_over_flag))
         p.start()
-        np.append(robots, p)
-    return robots_shm
+        processes.append(p)
+
+    return robots_shm, processes
 
 if __name__ == "__main__":
     grid_shm = create_grid()
-    robots_shm = spawn_robots(4) 
+    robots_shm, processos = spawn_robots(4)
     try:
         viewer(linhas, colunas, grid_shm)
+
+        while game_over_flag.value == 0:
+            time.sleep(1)
     except KeyboardInterrupt:
+        print("Encerrando o jogo manualmente")
+    finally:
         grid_shm.close()
         grid_shm.unlink()
         robots_shm.close()
         robots_shm.unlink()
+
+        for p in processos:
+            p.terminate()
