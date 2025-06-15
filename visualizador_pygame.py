20467fe414be1955b363194ea44ce3b76c0645c4
Thiago Ribeiro <thiagogr71@gmail.com>
2025-06-15 11:33:08 -0300

    Make player move

--- visualizador_pygame.py
+++ visualizador_pygame.py
@@ -1,3 +1,4 @@
+from multiprocessing import shared_memory
 import pygame
 import numpy as np
 
@@ -12,12 +13,17 @@ CORES = {
     10: (255, 0, 0),     # robô comum - vermelho
 }
 
-def cor_por_valor(valor):
-    return CORES.get(valor, (100, 100, 100)) 
+robot_dtype = np.dtype([
+        ('id', np.int32),
+        ('strength', np.int32),
+        ('energy', np.int32),
+        ('speed', np.int32),
+        ('pos', np.int32, (2,)),
+        ('status', np.int8),
+        ('type', np.int8)
+    ])
 
-def viewer(linhas, colunas, shm):
-    tabuleiro_shm = np.ndarray((linhas, colunas), dtype=np.int8, buffer=shm.buf)
-
+def viewer(linhas, colunas, grid_shm, robots_shm):
     pygame.init()
     screen = pygame.display.set_mode((colunas * largura_bloco, linhas * altura_bloco))
     pygame.display.set_caption("Visualização do Tabuleiro")
@@ -25,16 +31,38 @@ def viewer(linhas, colunas, shm):
     clock = pygame.time.Clock()
     rodando = True
     while rodando:
+        tabuleiro_shm = np.ndarray((linhas, colunas), dtype=np.int8, buffer=grid_shm.buf)
+        robots = np.ndarray((4,), dtype=robot_dtype, buffer=robots_shm.buf)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 rodando = False
-        # Checa shm se flag game_over === True
+            if event.type == pygame.KEYDOWN:
+                print(robots)
+                player = [x for x in robots if x['type'] == 99][0]
+                x, y = player['pos'][0], player['pos'][1]
+                novo_x, novo_y = x, y
+                if event.key == pygame.K_UP:
+                    novo_y = max(0, y - 1)
+                elif event.key == pygame.K_DOWN:
+                    novo_y = min(linhas - 1, y + 1)
+                elif event.key == pygame.K_LEFT:
+                    novo_x = max(0, x - 1)
+                elif event.key == pygame.K_RIGHT:
+                    novo_x = min(colunas - 1, x + 1)
+                print(player)
+                if tabuleiro_shm[novo_y, novo_x] != 1:
+                    tabuleiro_shm[y, x] = 0       
+                    tabuleiro_shm[novo_y, novo_x] = 99
+                    player['pos'][0] = novo_x
+                    player['pos'][1] = novo_y
+                    print(f"Moved to ({novo_x}, {novo_y})")
+            # Checa shm se flag game_over === True
 
         screen.fill((200, 200, 200))
         for i in range(linhas):
             for j in range(colunas):
                 valor = tabuleiro_shm[i, j]
-                cor = cor_por_valor(valor)
+                cor = CORES.get(valor, (100, 100, 100)) 
                 pygame.draw.rect(screen, cor,
                     (j * largura_bloco, i * altura_bloco, largura_bloco, altura_bloco))
         
